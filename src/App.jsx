5d674be76a5c867935b457647823bfb2491f4e7c
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-08-21 23:00:08 +0330

    building the layout

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+export default function App() {
+  return (
+    <div className='app'>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className='add-form'>
+      <h3>what do you need for your trip 😍 ?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className='list'>LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className='stats'>
+      <em>
+        💼 You have X items on your list, and you already have X (X%)
+      </em>
+    </footer>
+  );
+}
